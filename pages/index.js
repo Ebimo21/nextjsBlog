27d7df8659b32b15791b87a3bf10dc2004fde6dd
ebimo21 <ebi4jah15@gmail.com>
2022-05-31 15:00:10 +0100

    update api call link from local host to https://nextjs-blog-tau-seven-60.vercel.app/

--- pages/index.js
+++ pages/index.js
@@ -102,7 +102,7 @@ export default function Home({postsData}) {
 
 export const getServerSideProps= async() =>{
 
-  const response = await fetch('http://localhost:3000/api/postapi');
+  const response = await fetch('https://nextjs-blog-tau-seven-60.vercel.app/api/postapi');
   // const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
   const postsData = await response.json();
 
--- pages/posts.js
+++ pages/posts.js
@@ -37,7 +37,7 @@ const posts = ({postsData}) => {
 
 export const getServerSideProps= async() =>{
 
-    const response = await fetch('http://localhost:3000/api/postapi');
+    const response = await fetch('https://nextjs-blog-tau-seven-60.vercel.app/api/postapi');
     // const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
     const postsData = await response.json();
   
