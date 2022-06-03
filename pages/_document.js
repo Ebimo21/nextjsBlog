04b2a86bcec11abf99bac7872303fb6f38925814
ebimo21 <ebi4jah15@gmail.com>
2022-06-03 13:17:28 +0100

    kjh

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { useState, useEffect } from "react";
+import {  useEffect } from "react";
 
 
 export default function Document(){
 
-    const [darkTheme, setDarkTheme] = useState(false);
-    const themeColor = darkTheme? 'dark': '';
-    useEffect(() => {
-        
     
-      
-    }, [darkTheme])
     
     return (
-        <Html className={themeColor}>
+        <Html >
             <Head />
-                <body className={`dark:bg-black dark:text-white`}>
+                <body>
                     <Main />
                     <NextScript />
                 </body>
